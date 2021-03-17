dfeb76c9745bac76e07a0a32a9a706a95b5d561c
root <root@master.(none)>
2021-03-17 21:02:04 +0000

    Remove test2

--- IOR/test.sh
+++ IOR/test.sh
@@ -1,6 +1,6 @@
 DATE=`date "+%b%d"`
 mpiexec -host master IOR -b 4m -t 4m -i 4 > ./Tests/"${DATE} - IOR Test $1 - 1 Node.txt"
 mpiexec -host master,node001 IOR -b 4m -t 4m -i 4 > ./Tests/"${DATE} - IOR Test $1 - 2 Node.txt"
-mpiexec -host master,node001,node002,node003 IOR b -b 4m -t 4m -i 4 > ./Tests/"${DATE} - IOR Test $1 - 4 Node.txt"
+mpiexec -host master,node001,node002,node003 IOR -b 4m -t 4m -i 4 > ./Tests/"${DATE} - IOR Test $1 - 4 Node.txt"
 mpiexec -host master,node001,node002,node003,node004,node005 IOR -b 4m -t 4m -i 4 > ./Tests/"${DATE} - IOR Test $1 - 6 Node.txt"
 mpiexec -host master,node001,node002,node003,node004,node005,node006,node007 IOR -b 4m -t 4m -i 4 > ./Tests/"${DATE} - IOR Test $1 - 8 Node.txt"
--- NPB/test2.sh
+++
@@ -1,7 +0,0 @@
-DATE=`date "+%b-%d"`
-
-mpiexec -host master ep.A.1 > ./Tests/"Mar16 - NPB Test $1 - 1 Node.txt"
-mpiexec -host master,node001 ep.A.2 > ./Tests/"Mar16 - NPB Test $1 - 2 Node.txt"
-mpiexec -host master,node001,node002,node003 ep.A.4 > ./Tests/"Mar16 - NPB Test $1 - 4 Node.txt"
-mpiexec -host master,node001,node002,node003,node004,node005 ep.A.6 > ./Tests/"Mar16 - NPB Test $1 - 6 Node.txt"
-mpiexec -host master,node001,node002,node003,node004,node005,node006,node007 ep.A.8 > ./Tests/"Mar16 - NPB Test $1 - 8 Node.txt"
